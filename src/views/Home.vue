20989097dc378087b54f535db220d16aa8aa6d34
Ben Stiff <ben.stiff@visionhealth.co.uk>
2023-06-14 16:14:35 +0100

    Chore: First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <BenTest />
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import BenTest from '@/components/BenTest.vue'
+</script>
